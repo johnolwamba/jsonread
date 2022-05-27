fe23aa026ae862eff88aeefb7314d2ef546496ff
Johnstone Ananda <johnolwamba@gmail.com>
2022-05-27 14:50:36 +0300

    first commit

---
+++ JSONRead/PeopleDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  PeopleDetailsViewController.h
+//  JSONRead
+//
+//  Created by Johnstone Ananda on 5/1/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PeopleDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JSONRead/PeopleDetailsViewController.m
@@ -0,0 +1,31 @@
+//
+//  PeopleDetailsViewController.m
+//  JSONRead
+//
+//  Created by Johnstone Ananda on 5/1/22.
+//
+
+#import "PeopleDetailsViewController.h"
+
+@interface PeopleDetailsViewController ()
+
+@end
+
+@implementation PeopleDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
